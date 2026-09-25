8872f154c72c549ae85478cac633c08abffe7818
Carlos Intrieri <cjdintrieri@gmail.com>
2026-09-25 08:05:54 -0300

    feat: Classe principal do Spring Boot

---
+++ backend/src/main/java/com/tecsys/gatewayplanner/GatewayPlannerApplication.java
@@ -0,0 +1,12 @@
+package com.tecsys.gatewayplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayPlannerApplication.class, args);
+    }
+}
